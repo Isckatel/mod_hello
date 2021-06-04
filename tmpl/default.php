999cf665fa8ec62ed2a4a81ba496a3b111ed4b69
Dom\Ivan <nt-polza@yandex.ru>
2021-06-04 15:32:53 +0500

    Стили

--- tmpl/default.php
+++ tmpl/default.php
@@ -3,18 +3,47 @@
 // No direct access
 defined('_JEXEC') or die; ?>
 <?php
-echo "Герои <br>"; //переменнаяя hello находится в области видимости mod_helloworld.php
 //переменнаяя hello находится в области видимости mod_helloworld.php
 if($hello)
 {
     $rows = count($hello); // количество полученных строк
-    echo "<table><tr><th>№</th><th>Имя</th><th>Роль</th><th>Здоровье</th><th>Мана</th><th>Атака</th><th>Скор. атаки</th><th>Скор. движ.</th><th>Физ. защита</th><th>Маг. защита</th><th>Восст. здоровья</th><th>Восст. маны</th></tr>";
+	echo "
+	<style>
+	  .tabheros {
+		border: 1px solid #eee;
+		//table-layout: fixed;
+		width: 100%;
+		margin-bottom: 20px;
+	  }
+	  .tabheros th {
+		font-weight: bold;
+		padding: 5px;
+		background: #efefef;
+		border: 1px solid #dddddd;
+	  }
+	  .tabheros td {
+		padding: 5px 10px;
+		border: 1px solid #eee;
+		text-align: left;
+	  }
+	  .tabheros tr:nth-child(odd){
+		background: #F7F7F7;
+	  }
+	  .tabheros tr:nth-child(even){
+		background: #fff;
+	  }
+
+	</style>
+	";
+    echo "<table class='tabheros'><tr><th>№</th><th>Имя</th><th>Роль</th><th>Здоровье</th><th>Мана</th><th>Атака</th><th>Скор. атаки</th><th>Скор. движ.</th><th>Физ. защита</th><th>Маг. защита</th><th>Восст. здоровья</th><th>Восст. маны</th></tr>";
     for ($i = 0 ; $i < $rows ; ++$i)
     {
         $row = $hello[$i];
+		$n = count($row);
+		$np = $i + 1;
         echo "<tr>";
-			echo "<td>$i</td>";
-            for ($j = 0 ; $j < 12 ; ++$j) echo "<td>$row[$j]</td>";
+			echo "<td>$np</td>";
+            for ($j = 0 ; $j < $n ; ++$j) echo "<td>$row[$j]</td>";
         echo "</tr>";
     }
     echo "</table>";
